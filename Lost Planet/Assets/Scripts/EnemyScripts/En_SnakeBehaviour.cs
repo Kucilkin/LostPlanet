f0afb8258f0e0ab316062a824be0a92d68002913
Kuvin <kuvindumb@gmail.com#>
2021-07-27 14:46:57 +0200

    Created Project
    Initial Push

---
+++ Lost Planet/Assets/Scripts/EnemyScripts/En_SnakeBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class En_SnakeBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    protected float maxHP = 5f;
+    protected float currHP;
+    [SerializeField]
+    protected float moveSpeed;
+    protected float damage;
+
+    protected Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        //maxHP = 3f;
+        currHP = maxHP;
+        damage = 8f;
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if(collision.gameObject.tag == "Player")
+            //Execute Player's "Damaged" Method
+    }
+
+    public void GetDamaged(float _damage)
+    {
+        currHP -= _damage;
+        if (currHP <= 0)
+            Destroy(gameObject);
+            //Execute "Dying" Method
+    }
+    protected virtual void Movement()
+    {
+        rb.MovePosition(transform.position - transform.right * moveSpeed);
+    }
+}
---
+++ Lost Planet/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement:
+    public float Speed; // Playerspeed
+    private float xInput; // Spielereingabe
+    private float towardsY = 0f; // Winkel zu dem dem sich der PLayer um eigene Achse dreht
+    //Springen:
+    public float JumpForce; //Sprungkraft
+    public int MaxJumps; //maximale Sprünge
+    private int jumpCounter; //Sprungzähler
+
+    public Rigidbody2D RB;
+    public GameObject playerModel;
+    //Ground:
+    public LayerMask GroundLayer;
+    public Vector2 CheckBox; 
+    public Transform FeetTrans;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal"); //Eingabesignal fürs laufen
+        //GroundCheck(); // GroundCheck aufrufen
+
+        //Drehen:
+        //if (xInput > 0f) // nach rechts gehen
+        //    towardsY = 0f;
+        //else if (xInput < 0f) // nach links gehen
+        //    towardsY = 180f;
+
+        //playerModel.transform.rotation = Quaternion.Lerp(playerModel.transform.rotation, Quaternion.Euler(0f, towardsY, 0f), Time.deltaTime * 10f);
+
+        //Springen:
+        if (Input.GetAxisRaw("Jump") > 0f && jumpCounter > 0)
+        {
+            Vector2 JumpPower = RB.velocity;
+            JumpPower.y = JumpForce;
+            RB.velocity = JumpPower;
+            jumpCounter--;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(xInput != 0)
+        RB.velocity = new Vector2(xInput * Speed, RB.velocity.y); //Vorfärtsbewegung
+    }
+
+    //Groundcheck:
+    //void GroundCheck()
+    //{
+    //    Collider2D checkBox = Physics2D.OverlapBox(FeetTrans.position, CheckBox, 1, GroundLayer);
+    //    if (checkBox)
+    //    {
+    //        jumpCounter = MaxJumps;
+    //    }
+    //}
+
+    //GroundcheckBox:
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(FeetTrans.position, CheckBox);
+    //}
+}
