cfda8b2beb21fb971289c1bff4cb504a332064cc
Kuvin <kuvindumb@googlemail.com>
2021-07-29 18:13:10 +0200

    Attempted repair

--- Lost Planet/Assets/Scripts/EnemyScripts/En_SnakeBehaviour.cs
+++ Lost Planet/Assets/Scripts/EnemyScripts/En_SnakeBehaviour.cs
@@ -4,44 +4,30 @@ using UnityEngine;
 
 public class En_SnakeBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    protected float maxHP = 5f;
-    protected float currHP;
-    [SerializeField]
-    protected float moveSpeed;
-    protected float damage;
 
-    protected Rigidbody2D rb;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
-    private void Start()
-    {
-        //maxHP = 3f;
-        currHP = maxHP;
-        damage = 8f;
-    }
+    
     private void FixedUpdate()
     {
         Movement();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if(collision.gameObject.tag == "Player")
-            //Execute Player's "Damaged" Method
+        if (collision.gameObject.tag == "Player")
+            collision.gameObject.GetComponent<HealthSystem>().GetDamaged(GetComponent<EnemyBase>().Damage) ;
     }
 
-    public void GetDamaged(float _damage)
-    {
-        currHP -= _damage;
-        if (currHP <= 0)
-            Destroy(gameObject);
-            //Execute "Dying" Method
-    }
+    
     protected virtual void Movement()
     {
-        rb.MovePosition(transform.position - transform.right * moveSpeed);
+        //Vector2 moveVelocity = new Vector2()
+        rb.MovePosition(transform.position - transform.right * GetComponent<EnemyBase>().moveSpeed);
+        //rb.AddForce(GetComponent<EnemyBase>().moveSpeed)
+
     }
 }
