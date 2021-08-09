bd733608d86c34dc6e12a00701f02d48efd4e3e4
Kuvin <kuvindumb@googlemail.com>
2021-08-09 17:51:34 +0200

    Basically finished the Health Bar and inserted first set of Playersprite
    Health bar now implemented and working. Playersprite set for running added.

--- Lost Planet/Assets/Scripts/Managing Scripts/HealthSystem.cs
+++ Lost Planet/Assets/Scripts/Managing Scripts/HealthSystem.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField]
-    private float maxHP;    //Maximum HP  
-    private float currHP;   //Current HP
+    public float maxHP;    //Maximum HP  
+    public float currHP;   //Current HP
+    public GameObject HealthBar;
 
     private void Start()
     {
         currHP = maxHP;
+        if (this.gameObject.tag == "Player")
+            HealthBar.GetComponent<HealthBar>().SetMaxHealth(maxHP);
     }
     /// <summary>
     /// Subtracts _damage value from current HP of the entity this method gets called by
@@ -20,6 +23,8 @@ public class HealthSystem : MonoBehaviour
     public void GetDamaged(float _damage)
     {
         currHP -= _damage;
+        if (this.gameObject.tag == "Player")
+            HealthBar.GetComponent<HealthBar>().UpdateHealth(currHP);
         if (currHP <= 0)
             Die();
     }
